e595c6cf2b49697b16fe88e7c47bd6c02a5384d4
psychobolt <psychobolt@gmail.com>
2017-09-29 09:48:43 -0700

    Integrate demo with react-hot-loader

--- app/index.js
+++ app/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 
 import {Application} from './app';
 
-ReactDOM.render(
-  <Application />,
-  document.getElementById('root')
-);
+const render = Component => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root')
+  );
+}
+
+render(Application);
+
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    const NextApp = require('./app').Application;
+    render(NextApp);
+  });
+}
